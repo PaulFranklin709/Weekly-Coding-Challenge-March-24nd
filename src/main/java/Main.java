26fc0807542ed5c3df86b618ee9c04832f433202
Paul Franklin <paul219@revature.net>
2023-03-23 17:30:48 -0400

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isSameNum(4,8));
+
+        System.out.println(isSameNum(2,2));
+
+        System.out.println(isSameNum(42,32));
+    }
+
+    public static boolean isSameNum(int x, int y) {
+        return x == y;
+    }
+}
